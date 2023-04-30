b83e3e32761e2af20906d763712725736ec2b619
piotrekSy <piotrsyjud@gmail.com>
2023-04-30 02:33:02 +0200

    lazyloading-added

--- src/components/Body/BodyContent/BodyContent.js
+++ src/components/Body/BodyContent/BodyContent.js
@@ -2,43 +2,57 @@ import './styles.scss';
 import { useState, useEffect } from 'react'
 import CircularProgress from '@mui/material/CircularProgress';
 import { texts } from './texts.js';
+import LazyLoad from 'react-lazyload';
 
 const BodyContent = () => {
-
-    const [products, setProducts] = useState();
+    //data:
+    const [products, setProducts] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
+    //pagination: 
+
+
 
     useEffect(() => {
-        fetch("https://dummyjson.com/products")
-            .then((res) => res.json())
-            .then((json) => {
-                setProducts(json);
-                setIsLoaded(true);
-            });
+        try {
+            fetch("https://dummyjson.com/products")
+                .then((res) => res.json())
+                .then((json) => {
+                    setProducts(json);
+                    setIsLoaded(true);
+                });
+        } catch (err) {
+            console.error(err)
+        }
     }, []);
 
-    console.log(products)
     return (
         <div className="bodyContainer">
-            {isLoaded ? <div>
-                {products.products.map((item) =>
-                    <div key={item.id}>
-                        <img src={item.thumbnail} alt={item.title} />
-                        {isLoaded ? item.images.map((item, index) =>
-                            <img src={item.images[index]}
-                                alt={item.images[index]} />)
-                            : null}
-                        <div>{texts.title}{item.title}</div>
-                        <div>Brand: {item.brand}</div>
-                        <div>Category: {item.category}</div>
-                        <div>Description:  {item.description}</div>
-                        <div>Price: {item.price}</div>
-                        <div>Discount % : {item.price}</div>
-                        <div>Available: {item.stock}</div>
-                        <div>Rating: {item.rating}</div>
-                    </div>)}
-            </div>
-                : <CircularProgress />}
+            {isLoaded ?
+                <div className="itemContainer" >
+                    {products.products.map((item) =>
+                        <div key={item.id} images={item.images} className="productCard" category={item.category}>
+                            <div className="productImage">
+                                <LazyLoad height={10}>
+                                    <img src={item?.thumbnail} alt={item.title} className="thumbnail" />
+                                </LazyLoad>
+                            </div>
+                            <div className="productDescription">
+                                <div>{texts.product}{item.title}</div>
+                                <div>{texts.brand} {item.brand}</div>
+                                {/* <div>{texts.category} {item.category}</div> */}
+                                {/* <div>{texts.description} {item.description}</div> */}
+                                <div>{texts.price} {item.price}</div>
+                                <div>{texts.discount} {item.price}</div>
+                                <div className="bottomInfo">
+                                    <div>{texts.available} {item.stock}</div>
+                                    <div className="productRating">{texts.rating}{item.rating}</div>
+                                </div>
+                            </div>
+                        </div>)}
+                </div>
+                : <div className="loaderContainer">
+                    <CircularProgress />
+                </div>}
         </div>
     )
 };
--- src/components/Body/BodyContent/texts.js
+++ src/components/Body/BodyContent/texts.js
@@ -1,10 +1,10 @@
 export const texts = {
-    title: "title",
-    brand: "brand",
-    category: "category",
-    description: "description",
-    price: "price",
-    discount: "discount",
-    available: "available",
-    rating: "rating",
+    product: "Product: ",
+    brand: "Brand: ",
+    category: "Category: ",
+    description: "Description: ",
+    price: "Price: ",
+    discount: "Discount: ",
+    available: "Stock: ",
+    rating: "Rating: ",
 }
